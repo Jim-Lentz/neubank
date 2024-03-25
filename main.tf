a68ca60a0aa6e7adbacdcdf1017e517f91ec0315
Jim Lentz <jim_lentz@outlook.com>
2024-03-24 23:21:20 -0700

    bug fix

--- main.tf
+++ main.tf
@@ -20,8 +20,8 @@ provider "azurerm" {
 
 # Create resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "dev-rgp-cis-neubank-use-001"
-  location = "East US"
+  name     = var.resource_group_name
+  location = var.location
 
   tags = {
     Environment = "Dev"
