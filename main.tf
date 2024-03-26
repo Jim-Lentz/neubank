52eb59f0eefad9d134ab4f6d185ab8e5ca188081
Jim Lentz <jim_lentz@outlook.com>
2024-03-26 11:58:11 -0700

    adding frontend

--- main.tf
+++ main.tf
@@ -40,8 +40,8 @@ module "networking" {
  module "frontend" {
   source              = "./modules/frontend"
   name                = "frontend-app"
-  resource_group_name = var.resource_group_name
-  location            = var.location
+  resource_group_name = "dev-rgp-cis-neubank-use-001" #var.resource_group_name
+  location            = "East US" #var.location
   app_service_plan_id = "placeholder" #azurerm_app_service_plan.asp.id # Define this resource or pass the ID if existing
   subnet_id           = module.networking.frontend_subnet_id #module.my_module.azurerm_subnet.subnet["frontend"].id # module.network.frontend_subnet_id # Ensure you have a network module or reference the subnet directly
   depends_on = [
