ff1abb6d6862bcadba05e61b43a7a24a386565bc
Jim Lentz <jim_lentz@outlook.com>
2024-03-29 07:37:31 -0700

    added NSGs

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -54,6 +54,110 @@ resource "azurerm_subnet" "back-end-subnet" {
   }
 }
 
+resource "azurerm_network_security_group" "backend_nsg" {
+  name                = "backend-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
+
+# Allow SQL traffic from the frontend subnet
+resource "azurerm_network_security_rule" "allow_sql" {
+  name                        = "allow-sql"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "1433"
+  source_address_prefix       = azurerm_subnet.front-end-subnet.address_prefixes[0]
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.backend_nsg.name
+}
+
+# Allow API traffic from the frontend subnet
+resource "azurerm_network_security_rule" "allow_api" {
+  name                        = "allow-api"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "8080"
+  source_address_prefix       = azurerm_subnet.front-end-subnet.address_prefixes[0]
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.backend_nsg.name
+}
+
+# Deny all other inbound traffic
+resource "azurerm_network_security_rule" "deny_all_inbound-backend" {
+  name                        = "deny-all-inbound-backend"
+  priority                    = 900
+  direction                   = "Inbound"
+  access                      = "Deny"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.backend_nsg.name
+}
+
+# NSG for the front end that only allows port 80 and 443 from the Internet
+resource "azurerm_network_security_group" "frontend_nsg" {
+  name                = "frontend-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
+
+# Allow SQL traffic from the Internet to port 80
+resource "azurerm_network_security_rule" "allow_http" {
+  name                        = "allow-http"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.frontend_nsg.name
+}
+
+# Allow API traffic from the Internet to port 443
+resource "azurerm_network_security_rule" "allow_https" {
+  name                        = "allow-https"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.frontend_nsg.name
+}
+
+# Deny all other inbound traffic
+resource "azurerm_network_security_rule" "deny_all_inbound-frontend" {
+  name                        = "deny-all-inbound-frontend"
+  priority                    = 900
+  direction                   = "Inbound"
+  access                      = "Deny"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.frontend_nsg.name
+}
+
+
 output "back-end-subnet" {
   value = azurerm_subnet.back-end-subnet.id
 }
